190e5a648e1254036714ca64e5af5560bd52ec2b
Jun Egawa <123811944+junenu@users.noreply.github.com>
2024-01-13 08:49:28 +0000

    Add:For1.cpp

---
+++ book/For1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 0;
+    for(int i = 0;i < 10;++i){
+        cout << "num * 2 =" << num * 2 << endl;
+        ++num;
+    }
+}
