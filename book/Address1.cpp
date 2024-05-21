ca56f1e1c73e90f0f9dd0d88b0c3c5bb0e1f36be
junenu <junenu41@gmail.com>
2024-05-21 22:07:24 +0900

    chore(book): add Address1.cpp

---
+++ book/Address1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void
+Foo(){
+    int a;
+    char b[10];
+    cout << "a   : " << (size_t)&a  << endl
+         << "b   : " << (size_t)b   << endl
+         << "Foo : " << (size_t)Foo << endl;
+}
+
+int
+main(){
+    Foo();
+}
